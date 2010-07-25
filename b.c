741c2cde5722605660d83363b538d795e5abe939
Ondrej Certik <ondrej@certik.cz>
2010-07-25 13:27:05 -0700

    Refactor the package
    Introduce a header file.

--- b.c
+++ b.c
@@ -1,8 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void show_backtrace (void);
-void print_stack_on_segfault();
+#include "stacktrace.h"
 
 void g()
 {
---
+++ stacktrace.h
@@ -0,0 +1,7 @@
+#ifndef _STACKTRACE_H_
+#define _STACKTRACE_H_
+
+void show_backtrace();
+void print_stack_on_segfault();
+
+#endif
